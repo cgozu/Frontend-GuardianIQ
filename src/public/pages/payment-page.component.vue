04a15d6343644b83d1c83dbd4aac38a970c4565d
Seba <sebalobato05@gmail.com>
2024-06-09 01:01:23 -0500

    Create payment-page.component.vue

---
+++ src/public/pages/payment-page.component.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import navbarComponent from "@/public/components/navbar/navbar.component.vue";
+import paymentComponent from "@/public/components/payment/payment.vue";
+</script>
+
+<template>
+
+  <navbar-component />
+  <paymentComponent></paymentComponent>
+
+</template>
+
+<style scoped>
+
+</style>
